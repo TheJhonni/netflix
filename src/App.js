6f82811d94b01c1f7bc33f4a197bbf4522946927
TheJhonni <davideconi91@gmail.com>
2022-02-10 20:19:48 +0100

    subscribe - unsuscribe !

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect =
+    (() => {
+      const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+        if (userAuth) {
+          //logged in!
+          console.log(userAuth);
+        } else {
+          //logged out!
+        }
+      });
+      return unsubscribe;
+    },
+    []);
+
   return (
     <BrowserRouter>
       <div className="app">
--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -24,6 +24,18 @@ function SignUpScreen() {
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
